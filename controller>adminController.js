0e4d5b14f8762a4b1d02edfd89198b557d8f7052
GPL6009 <77183034+GPL6009@users.noreply.github.com>
2022-12-20 17:35:41 +0530

    Update controller>adminController.js

--- controller>adminController.js
+++ controller>adminController.js
@@ -3,51 +3,76 @@ const database = require('../controller/database');
 exports.add_user = function (req, res) {
     //console.log('Form data:',req.body);
 
-    var first_name = req.body.first_name;
+    //console.log('Form data:',req.body.first_name);
 
-    var last_name = req.body.last_name;
+    //var first_name = req.body.first_name;
 
-    var number = req.body.number;
-
+    //var data     = first_name.split(',')[1];
+    //console.log('already..!');
+        //var eval=data[0].email;
+        //console.log(eval);
+    //var eval;
     var email = req.body.email;
 
-    var gender = req.body.gender;
+    var query='SELECT * FROM users WHERE email ="' + email + '"';
+    database.query(query, function(err, result){
+        if (err) throw err;
+        //console.log('already..!');
+        //var eval=0;
+        //var eval=data[0].email;
+        //console.log(eval);
+        
+        if(result.length===0){
+            var first_name = req.body.first_name;
 
-    var address = req.body.address;
+        var last_name = req.body.last_name;
 
-    var password = req.body.password;
+        var number = req.body.number;
 
-    var cpassword = req.body.cpassword;
+        var email = req.body.email;
 
-    var myimage = req.body.myimage;
+        var gender = req.body.gender;
 
-    var query = `
-    INSERT INTO users 
-    (first_name, last_name, number, email, gender, address, password, cpassword, profile) 
-    VALUES ("${first_name}", "${last_name}", "${number}", "${email}", "${gender}", "${address}", "${password}", "${cpassword}", "${myimage}")
-    `;
+        var address = req.body.address;
 
-    database.query(query, function(error, data)	{
-        if(data<0){
-            console.log('no data');
+        var password = req.body.password;
+
+        var cpassword = req.body.cpassword;
+
+        var myimage = "hi";
+
+        //var file = req.file.myimage;
+
+        //var img_name=file.name;
+
+        var query = `
+INSERT INTO users 
+(first_name, last_name, number, email, gender, address, password, cpassword, profile) 
+VALUES ("${first_name}", "${last_name}", "${number}", "${email}", "${gender}", "${address}", "${password}", "${cpassword}", "${myimage}")
+`;
+
+        database.query(query, function (error, data) {
+            if (data < 0) {
+                console.log('no data');
+            }
+            if (error) {
+
+                throw error;
+            }
+            else {
+                
+        return res.send(console.log("inserted"));
+            }
+            //res.send(console.log('wow you done..!'));
+            
+        });
+        
         }
-    if(error){
-
-        throw error;
-    }
-	else
-	{
-		console.log('inserted');
-	}
-    res.send(console.log('wow you done..!'));
+        else{
+            //console.log('This email is already exists..!');
+            return res.send(console.log("This email is already exists..!"));
+        }
+        
     });
-
-
-
-
-
-
-
-
     
 }
